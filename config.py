e0043f49bff11ff1e3abe86aa35dfbfae4f21383
Shivam Krishnan <127594902+shivamKrishnan@users.noreply.github.com>
2026-04-07 09:40:54 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+# Environment variables
+BOT_TOKEN = ""
+CHAT_ID = ""
+
+# Database
+DB_NAME = "rss_bot.db"
+
+# RSS Feeds (default)
+DEFAULT_RSS_FEEDS = ["http://rss.cnn.com/rss/edition.rss"]
